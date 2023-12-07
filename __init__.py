249834dc7b8cfd4eff8f02c591cd55852c3964c0
SimonNordon4 <simon.nordon@protonmail.com>
2023-12-07 21:29:45 +1000

    successfully registers the second time, but fails the first.

--- __init__.py
+++ __init__.py
@@ -11,6 +11,23 @@ bl_info = {
 import importlib
 import os
 import sys
+import bpy
+
+from bpy.types import (
+            Operator, 
+            PropertyGroup, 
+            Panel
+        )
+
+from bpy.props import (
+            IntProperty,
+            EnumProperty,
+            BoolProperty,
+            FloatProperty,
+            StringProperty,
+            PointerProperty,
+            CollectionProperty
+        )
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,17 +35,35 @@ if __name__ == "__main__":
     sys.path.insert(0, script_dir)
     __package__ = package_name
 
-from . import auto_load
 
+
+
+
+from . import auto_load
 if "auto_load" in locals():
     importlib.reload(auto_load)
+
+
+
+
 
 auto_load.AutoLoader.init()
 
 def register():
     auto_load.AutoLoader.register()
 
+    from . import bakelab_properties
+    bpy.types.Scene.BakeLabProps = PointerProperty(type = bakelab_properties.BakeLabProperties)
+    # bpy.types.Scene.BakeLabMaps = CollectionProperty(type = bakelab_map.BakeLabMap)
+    # bpy.types.Scene.BakeLab_Data = CollectionProperty(type = bakelab_baked_data.BakeLab_BakedData)
+    # bpy.types.Scene.BakeLabMapIndex = IntProperty(name = 'BakeLab Map List Index')
+
 def unregister():
+    # del bpy.types.Scene.BakeLabMapIndex
+    # del bpy.types.Scene.BakeLab_Data
+    # del bpy.types.Scene.BakeLabMaps
+    # del bpy.types.Scene.BakeLabProps 
+
     auto_load.AutoLoader.unregister()
 
 if __name__ == "__main__":
